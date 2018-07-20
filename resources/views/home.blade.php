8f7ee656ae1630dbf52ef455f0021a22d44b9c0d
Sergio Gordillo <gordillo78@gmail.com>
2018-07-20 00:57:40 -0300

    se cambio a castellano el mensaje de login correcto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Usted se ha logueado correctamente!!!!
                 </div>
             </div>
         </div>
